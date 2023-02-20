fc2b44bdcaeb988e4bd2d323a6e0d3a7fdde0bc2
MikeBeekeeper <mike.rotar.91@gmail.com>
2023-02-20 21:04:33 +0200

    add comment

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,5 +1,5 @@
 import Notiflix from 'notiflix';
-const axios = require('axios');
+// const axios = require('axios');
 const ENDPOINT = 'https://pixabay.com/api';
 const KEY = '33577731-7b9b7bf07a9d841c486c320f5';
 const searchOptions = 'image_type=photo&orientation=horizontal&safesearch=true';
