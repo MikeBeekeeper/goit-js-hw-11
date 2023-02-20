e7c9f19bdab2aa2185ace60d4ce359e281fa33cf
MikeBeekeeper <mike.rotar.91@gmail.com>
2023-02-20 20:59:58 +0200

    add axios in comment

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -4,27 +4,27 @@ const ENDPOINT = 'https://pixabay.com/api';
 const KEY = '33577731-7b9b7bf07a9d841c486c320f5';
 const searchOptions = 'image_type=photo&orientation=horizontal&safesearch=true';
 
-// export default async function fetchPhotos(query, page) {
-//     const URL = `${ENDPOINT}/?key=${KEY}&q=${query}&${searchOptions}&page=${page}&per_page=40`;
-//     const response = await fetch(URL);
-//     if (!response.ok) {
-//             Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
-//     }
-//     const photos = await response.json();
-//     return photos;
-// }
-
 export default async function fetchPhotos(query, page) {
     const URL = `${ENDPOINT}/?key=${KEY}&q=${query}&${searchOptions}&page=${page}&per_page=40`;
+    const response = await fetch(URL);
+    if (!response.ok) {
+            Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+    }
+    const photos = await response.json();
+    return photos;
+}
+
+// export default async function fetchPhotos(query, page) {
+//     const URL = `${ENDPOINT}/?key=${KEY}&q=${query}&${searchOptions}&page=${page}&per_page=40`;
     
-    return await axios.get(URL)
-.then(response => {
-if (response.status !== 200 || response.data.hits.length === 0) {
-throw new Error(response.status)
-}
-return response.data;
-})
-}
+//     return await axios.get(URL)
+// .then(response => {
+// if (response.status !== 200 || response.data.hits.length === 0) {
+// throw new Error(response.status)
+// }
+// return response.data;
+// })
+// }
 
 
 
